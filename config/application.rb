1c3833e16343bf503a989f7abf1febbf4298f601
BMuscle <rk221.work@gmail.com>
2021-07-05 10:31:04 +0900

    RSpecの追加(#1)

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module TrackerWeb
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.helper false
+      g.test_framework :rspec,
+                      view_specs: false,
+                      helper_specs: false,
+                      controller_specs: false,
+                      routing_specs: false
+    end
   end
 end
