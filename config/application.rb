6b9d65cb3d2fd9f73341d4b599b859f2603f8e19
BMuscle <rk221.work@gmail.com>
2021-07-07 10:00:32 +0900

    RailsのAPI化

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,19 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,12 +24,6 @@ module TrackerWeb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    yaml_file = YAML.safe_load(ERB.new(Rails.root.join('config/config.yml').read).result)['tracker']
-    if File.exist?(Rails.root.join('config/local_config.yml'))
-      yaml_file.merge!(YAML.load_file(Rails.root.join('config/local_config.yml'))['tracker'])
-    end
-    config.capybara_app_host = yaml_file['capybara_app_host']
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -26,13 +31,10 @@ module TrackerWeb
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.generators do |g|
-      g.helper false
-      g.test_framework :rspec,
-                       view_specs: false,
-                       helper_specs: false,
-                       controller_specs: false,
-                       routing_specs: false
-    end
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
