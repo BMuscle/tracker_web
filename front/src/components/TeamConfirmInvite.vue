158cdc23e70646ae222e6a31f907ee38e11e1478
BMuscle <rk221.work@gmail.com>
2021-07-25 21:17:42 +0900

    チーム招待確認時のtoast追加(#46)

--- front/src/components/TeamConfirmInvite.vue
+++ front/src/components/TeamConfirmInvite.vue
@@ -5,17 +5,37 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import axios from '@/plugins/axios'
+import ToastModule from '@/store/modules/toast'
 
 @Component
 export default class TeamConfirmInvite extends Vue {
+  pushNoticeSuccess (already: boolean): void {
+    const message = already
+      ? 'toast.particpate_already'
+      : 'toast.particpate_success'
+    ToastModule.pushNotice({
+      message: this.$t(message),
+      notificationType: 'success'
+    })
+  }
+
+  pushNoticeFailed (): void {
+    ToastModule.pushNotice({
+      message: this.$t('toast.particpate_failed'),
+      notificationType: 'danger'
+    })
+  }
+
   async created (): Promise<void> {
     try {
       const result = await axios.post('/teams/invites/confirm', {
         team: { guid: this.$route.query.guid }
       })
       this.$router.push(`/dashboard/teams/${result.data.id}`)
+      this.pushNoticeSuccess(result.data.already)
     } catch (err) {
       this.$router.push('/dashboard')
+      this.pushNoticeFailed()
     }
   }
 }
