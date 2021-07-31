5ade2e54e353c381d6c7b5b514630cd048d97ded
BMuscle <rk221.work@gmail.com>
2021-07-31 15:39:21 +0900

    UserInRoomChannelのテスト追加(#55)

--- spec/channels/user_in_room_channel_spec.rb
+++ spec/channels/user_in_room_channel_spec.rb
@@ -3,5 +3,57 @@
 require 'rails_helper'
 
 RSpec.describe UserInRoomChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    stub_connection
+  end
+
+  describe 'メッセージの送信' do
+    describe 'User#leave_room' do
+      let(:user_in_room) { create(:user_in_room, :other_room_and_user) }
+      let(:room) { user_in_room.room }
+      let(:team) { room.team }
+      let(:user) { user_in_room.user }
+
+      before do
+        subscribe(team_id: team.id)
+        user.leave_room
+      end
+
+      it '退出したことが送信されること' do
+        have_broadcasted_to("user_in_room_#{team.id}").with do |data|
+          expect(data['message']).to eq('leaved')
+        end
+      end
+
+      it 'そのチームのルームデータが送信されること' do
+        have_broadcasted_to("user_in_room_#{team.id}").with do |data|
+          expect(data['rooms']).to eq([{ 'id' => room.id, 'name' => room.name, 'users' => [] }])
+        end
+      end
+    end
+
+    describe 'Team.broadcast_rooms' do
+      let(:user_in_room) { create(:user_in_room, :other_room_and_user) }
+      let(:room) { user_in_room.room }
+      let(:team) { room.team }
+      let(:user) { user_in_room.user }
+
+      before do
+        subscribe(team_id: team.id)
+        Team.broadcast_rooms(team.id, 'participated')
+      end
+
+      it 'メッセージが送信されること' do
+        have_broadcasted_to("user_in_room_#{team.id}").with do |data|
+          expect(data['message']).to eq('leaved')
+        end
+      end
+
+      it 'そのチームのルームデータが送信されること' do
+        have_broadcasted_to("user_in_room_#{team.id}").with do |data|
+          expect(data['rooms']).to eq([{ 'id' => room.id, 'name' => room.name, 'users' => [] }])
+        end
+      end
+    end
+  end
 end
