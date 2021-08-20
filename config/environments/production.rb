c45de5c4f59d93935fc89b56f3d865abf5acc431
BMuscle <rk221.work@gmail.com>
2021-08-20 20:57:45 +0900

    Redisの追加(#21)
    本番環境用にRedisの追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.mount_path = '/cable'
+
+  # vueから通信しているので不要
+  # config.action_cable.url = "wss://#{config.front_url}/cable"
+
+  # writed application.rb
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
