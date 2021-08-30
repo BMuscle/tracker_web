5caad616245ce5d310155e69f05c1cf38c0330ae
BMuscle <rk221.work@gmail.com>
2021-08-30 11:37:45 +0900

    Agentの接続時にログを追加(#82)

--- app/controllers/agent_api/agent_controller.rb
+++ app/controllers/agent_api/agent_controller.rb
@@ -11,6 +11,8 @@ module AgentApi
     private
 
     def authenticate_agent
+      request.headers.sort.map { |k, v| logger.info "#{k}:#{v}" }
+      logger.info "Connect AgentGUID=#{request.headers['TRACKER_AGENT_GUID']}"
       @current_user = Agent.find_by!(agent_params).user
     end
 
